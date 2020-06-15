98e037ba35f000a997be9096daaa2fd130b502b5
Ngong Ivoline Clarisse <ivolinengong@gmail.com>
2020-06-15 11:11:13 +0300

    hippocampal segmentation

---
+++ src/inference/UNetInferenceAgent.py
@@ -0,0 +1,118 @@
+"""
+Contains class that runs inferencing
+"""
+import torch
+import numpy as np
+
+from networks.RecursiveUNet import UNet
+
+from utils.utils import med_reshape
+
+class UNetInferenceAgent:
+    """
+    Stores model and parameters and some methods to handle inferencing
+    """
+    def __init__(self, parameter_file_path='', model=None, device="cpu", patch_size=64):
+
+        self.model = model
+        self.patch_size = patch_size
+        self.device = device
+
+        if model is None:
+            self.model = UNet(num_classes=3)
+
+        if parameter_file_path:
+            self.model.load_state_dict(torch.load(parameter_file_path, map_location=self.device))
+
+        self.model.to(device)
+
+#     def single_volume_inference_unpadded(self, volume):
+#         """
+#         Runs inference on a single volume of arbitrary patch size,
+#         padding it to the conformant size first
+
+#         Arguments:
+#             volume {Numpy array} -- 3D array representing the volume
+
+#         Returns:
+#             3D NumPy array with prediction mask
+#         """
+        
+#         self.model.eval()
+
+#         # Assuming volume is a numpy array of shape [X,Y,Z] and we need to slice X axis
+#         slices = []
+#         volume = med_reshape(volume, new_shape=(volume.shape[0], 64, 64))
+#         slices = np.zeros(volume.shape)
+
+#         def inference(img):
+#             tsr_test = torch.from_numpy(img.astype(np.single)/np.max(img)).unsqueeze(0).unsqueeze(0)
+#             print(tsr_test.shape)
+#             pred = self.model(tsr_test.to(self.device))
+#             return np.squeeze(pred.cpu().detach())
+
+#         for slc_ix in range(volume.shape[0]):
+#             pred = inference(volume[slc_ix,:,:])
+#             slices[slc_ix,:,:] = torch.argmax(pred, dim=0)
+
+
+#         return slices
+
+    def single_volume_inference_unpadded(self, volume):
+        """
+        Runs inference on a single volume of arbitrary patch size,
+        padding it to the conformant size first
+
+        Arguments:
+            volume {Numpy array} -- 3D array representing the volume
+
+        Returns:
+            3D NumPy array with prediction mask
+        """
+        new_volume = np.moveaxis(volume, -1, 0)
+        print("new volume", new_volume.shape)
+        reshaped_volume = med_reshape(new_volume, new_shape=(new_volume.shape[0], self.patch_size, self.patch_size))
+        pred = self.single_volume_inference(reshaped_volume.astype(np.float32))
+        reshaped_pred = np.zeros_like(new_volume)
+        print("hey", reshaped_volume.shape, pred.shape, reshaped_pred.shape)        
+        resize_dim = [reshaped_pred.shape[2], reshaped_pred.shape[1]]
+        print("lala", resize_dim)
+        for i in range(reshaped_pred.shape[0]):
+            reshaped_pred[i] = Image.fromarray(pred[i].astype(np.uint8)).resize(resize_dim)
+            
+        return np.moveaxis(reshaped_pred, 0, -1)
+
+    def single_volume_inference(self, volume):
+        """
+        Runs inference on a single volume of conformant patch size
+
+        Arguments:
+            volume {Numpy array} -- 3D array representing the volume
+
+        Returns:
+            3D NumPy array with prediction mask
+        """
+        self.model.eval()
+
+        # Assuming volume is a numpy array of shape [X,Y,Z] and we need to slice X axis
+        slices = []
+
+        # TASK: Write code that will create mask for each slice across the X (0th) dimension. After 
+        # that, put all slices into a 3D Numpy array. You can verify if your method is 
+        # correct by running it on one of the volumes in your training set and comparing 
+        # with the label in 3D Slicer.
+        # <YOUR CODE HERE>
+        
+        # slicing the x axis
+        #test_tensor = torch.tensor(volume).unsqueeze(1)
+        # Assuming volume is a numpy array of shape [X,Y,Z] and we need to slice X axis
+#         outputs = self.model(test_tensor.float().to(self.device)).cpu().detach()
+#         _, slices = torch.max(outputs, 1)
+#         slices = slices.numpy
+        input_tensor = torch.tensor(volume).unsqueeze(1)
+        # Assuming volume is a numpy array of shape [X,Y,Z] and we need to slice X axis
+        outputs = self.model(input_tensor.float().to(self.device)).cpu().detach()
+        _, slices = torch.max(outputs, 1)
+    
+        return slices.numpy()
+        
---
+++ src/utils/volume_stats.py
@@ -0,0 +1,95 @@
+"""
+Contains various functions for computing statistics over 3D volumes
+"""
+import numpy as np
+
+def Dice3d(a, b):
+    """
+    This will compute the Dice Similarity coefficient for two 3-dimensional volumes
+    Volumes are expected to be of the same size. We are expecting binary masks -
+    0's are treated as background and anything else is counted as data
+
+    Arguments:
+        a {Numpy array} -- 3D array with first volume
+        b {Numpy array} -- 3D array with second volume
+
+    Returns:
+        float
+    """
+    if len(a.shape) != 3 or len(b.shape) != 3:
+        raise Exception(f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
+
+    if a.shape != b.shape:
+        raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
+
+    # TASK: Write implementation of Dice3D. If you completed exercises in the lessons
+    # you should already have it.
+    a = (a > 0.1)*1.
+    b = (b > 0.1)*1.
+    intersection = np.sum(a*b)
+    volumes = np.sum(a) + np.sum(b)
+
+    if volumes == 0:
+        return -1
+
+    return 2.*float(intersection) / float(volumes)
+
+def Jaccard3d(a, b):
+    """
+    This will compute the Jaccard Similarity coefficient for two 3-dimensional volumes
+    Volumes are expected to be of the same size. We are expecting binary masks - 
+    0's are treated as background and anything else is counted as data
+
+    Arguments:
+        a {Numpy array} -- 3D array with first volume
+        b {Numpy array} -- 3D array with second volume
+
+    Returns:
+        float
+    """
+    if len(a.shape) != 3 or len(b.shape) != 3:
+        raise Exception(f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
+
+    if a.shape != b.shape:
+        raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
+
+    # TASK: Write implementation of Jaccard similarity coefficient. Please do not use 
+    # the Dice3d function from above to do the computation ;)
+    # <YOUR CODE GOES HERE>
+    intersection = sum([1 if (a[x, y, z] != 0 and b[x, y, z] != 0) else 0 for x in range(a.shape[0]) for y in range(a.shape[1]) for z in range(a.shape[2])])
+    union = sum([1 if (a[x, y, z] != 0 or b[x, y, z] != 0) else 0 for x in range(a.shape[0]) for y in range(a.shape[1]) for z in range(a.shape[2])])
+
+    return intersection/union
+
+# def Jaccard3d(a, b):
+#     """
+#     This will compute the Jaccard Similarity coefficient for two 3-dimensional volumes
+#     Volumes are expected to be of the same size. We are expecting binary masks - 
+#     0's are treated as background and anything else is counted as data
+
+#     Arguments:
+#         a {Numpy array} -- 3D array with first volume
+#         b {Numpy array} -- 3D array with second volume
+
+#     Returns:
+#         float
+#     """
+#     if len(a.shape) != 3 or len(b.shape) != 3:
+#         raise Exception(f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
+
+#     if a.shape != b.shape:
+#         raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
+
+#     # TASK: Write implementation of Jaccard similarity coefficient. Please do not use 
+#     # the Dice3D function from above to do the computation ;)
+#     # <YOUR CODE GOES HERE>
+    
+#     a = (a > 0.1)*1.
+#     b = (b > 0.1)*1.
+#     intersection = np.sum(a*b)
+#     volumes = np.sum(a) + np.sum(b)
+
+#     if volumes == 0:
+#         return -1
+
+#     return float(intersection) / float(volumes)
